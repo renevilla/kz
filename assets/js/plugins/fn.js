8498940e2688c428ad177dd81c1322699ec779ac
Rene Villa <renevilla23@gmail.com>
2014-02-27 20:54:03 -0800

    updated js

--- assets/js/plugins/fn.js
+++ assets/js/plugins/fn.js
@@ -2,62 +2,20 @@
 /* click and load content
 * ======================================================================== */
 function displayContent() {
-  var $link1 = $('.row.nav li a.bio');
-  var $link2 = $('.row.nav li a.stylist');
-  var $link3 = $('.row.nav li a.contact');
-  var $content = $('#text-content')
-  var $bio = $("#bio");
-  var $stylist = $("#stylist");
-  var $contact = $("#contact");
-  var $overlay = $('.content-overlay');
-
-  //link1
-  $link1.click(function (e) {
+$('.row.nav li a').click(function(e) {
+    e.preventDefault();
     e.stopPropagation();
-    $link2.removeClass('active');
-    $link3.removeClass('active');
-    $link1.addClass('active');
-    $contact.hide();
-    $stylist.hide();
-    $bio.fadeIn(700);
-    $overlay.show();
-  });
-  //link2
-  $link2.click(function (e) {
-    e.stopPropagation();
-    $link1.removeClass('active');
-    $link3.removeClass('active');
-    $link2.addClass('active');
-    $bio.hide();
-    $contact.hide();
-    $stylist.fadeIn(700);
-    $overlay.show();
-  });
-  //link3
-  $link3.click(function (e) {
-    e.stopPropagation();
-    $link1.removeClass('active');
-    $link2.removeClass('active');
-    $link3.addClass('active');
-    $bio.hide();
-    $stylist.hide();
-    $overlay.show();
-    $contact.fadeIn(700);
-  });
+    var cls = $(this).prop('class');
+    $('#' + cls).fadeIn(700).siblings('div').hide();
+    $('.content-overlay').show();
+    $('.row.nav li a').removeClass('active');
+    $(this).addClass('active');
+});
 
-
-  //close overlay/hide content
-  $('html').click(function (e) {
-    e.stopPropagation();
-    $link1.removeClass('active');
-    $link2.removeClass('active');
-    $link3.removeClass('active');
-    $bio.fadeOut();
-    $stylist.fadeOut();
-    $overlay.fadeOut();
-    $contact.fadeOut();
-  });
-
+$('html').click(function () {
+    $('.row.nav li a').removeClass('active');
+    $("#bio, #stylist, #contact, .content-overlay").fadeOut();
+});
 }
 
 /* click and load video
